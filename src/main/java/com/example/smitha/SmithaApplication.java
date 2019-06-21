302cf65959901ce549ea2387f6dd24e4a844fdc2
unknown <sathisusmitha@gmail.com>
2019-06-21 15:38:19 +0530

    first commit

---
+++ src/main/java/com/example/smitha/SmithaApplication.java
@@ -0,0 +1,13 @@
+package com.example.smitha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmithaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmithaApplication.class, args);
+	}
+
+}
